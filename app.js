cfddb619ce0e1dbc59f19df99d9bb953688ce2ed
tusharnikam1 <tushar.nikam@globant.com>
2019-01-26 23:09:36 +0530

    Removed user routes

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users  = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -29,9 +26,6 @@ app.use(function(req, res, next){
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
