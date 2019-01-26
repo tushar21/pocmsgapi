b023a5051342951c0549cce1465878a35ecbfb59
tusharnikam1 <tushar.nikam@globant.com>
2019-01-26 21:56:53 +0530

    Added console log

--- models/index.js
+++ models/index.js
@@ -8,6 +8,9 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.js')[env];
 var db        = {};
 
+console.log(config, "config");
+console.log(process.env, "process env");
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
